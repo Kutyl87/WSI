5e699aa58b0eb984251b33db91c5f7f9930efc49
pkutyl <01168894@pw.edu.pl>
2023-10-31 14:41:29 +0100

    Clean code finish the algorithm

--- EvolutionStrategy/evolution_strategy.py
+++ EvolutionStrategy/evolution_strategy.py
@@ -9,26 +9,44 @@ from math import floor
 
 
 class EvolutionStrategy:
-    def __init__(self, x, y, func, mi, lamb, max_iter, seed=None):
+    def __init__(self, x, y, funct, mi, lamb, max_iter, seed=None):
+        """
+        Initializes an EvolutionStrategy object with specified parameters.
+        Args:
+            x - list of x values
+            y - list of y values
+            funct - the objective function to be optimized
+            mi - parent population size
+            lamb - offspring population size
+            max_iter - maximum number of iterations
+            seed - random number generator seed (optional)
+        """
         self._x = x
         self._y = y
         self._mi = mi
         self._lamb = lamb
         self._seed = seed if seed else random.randint(10, 101)
         self._logger = logging.getLogger(__name__)
-        self._func = func
+        self._func = funct
         self._offset = 0.005
         self._max_iter = max_iter
         self._paired_population = None
         self._new_generation = None
 
-    def fitness(self):
-        x = self._population[:, 0]
-        y = self._population[:, 1]
+    def fitness(self, population):
+        """
+        Calculates the fitness of a population by applying the objective function to x and y values in the population.
+        Args:
+            population - a population where each individual is described as a pair (x, y)
+        Returns:
+            Standardized fitness values of the population
+        """
+        x = population[:, 0]
+        y = population[:, 1]
         function_value = self._func(x, y)
-        standarized_function_value = (function_value - np.min(function_value) + self._offset) / np.sum(
+        standardized_function_value = (function_value - np.min(function_value) + self._offset) / np.sum(
             function_value - np.min(function_value) + self._offset)
-        return standarized_function_value
+        return standardized_function_value
 
     def set_starting_points(self, mi):
         """
@@ -44,40 +62,89 @@ class EvolutionStrategy:
         y = self._y[random_indexes[1][:]].reshape(mi, 1)
         starting_points = np.concatenate((x, y), axis=1)
         self._logger.info(f"Starting points: ({starting_points})")
-        self._population = starting_points
-        # return np.array([x, y]).reshape((2, 1))
+        return starting_points
 
+    def extend_population(self, parents):
+        """
+        Extends the population by repeating parents.
+        Args:
+            parents - parent population
+        Returns:
+            Extended population
+        """
+        return np.repeat(parents, self._lamb, axis=0)
 
-    def extend_population(self):
-        self._paired_population = np.repeat(self._paired_population, self._lamb, axis= 0)
+    def crossover(self, parents):
+        """
+        Performs crossover operation (not fully implemented).
+        Args:
+            parents - parent population
+        Returns:
+            Offspring population
+        """
+        a = np.repeat(np.random.normal(0, 1, size=(floor(self._mi / 2) * self._lamb, 1)), 2, axis=1)
+        parents_extended = self.extend_population(parents)
+        return parents_extended[:, 0, :] * a + parents_extended[:, 1, :] * (1 - a)
 
-    def crossover(self):
-        a = np.repeat(np.random.normal(0, 1, size=(floor(self._mi/2) * self._lamb, 1)), 2, axis=1)
-        # print(a.shape)
-        self.extend_population()
-        # print(self._population)
-        # print(self._paired_population)
-        print(self._paired_population.shape)
-        self._new_generation = self._paired_population[:,0,:] * a + self._paired_population[:,1,:] * (1-a)
-        print(self._new_generation.shape)
-        # print(a.shape)
+    @staticmethod
+    def selection(population, probabilities):
+        """
+        Performs selection of individuals from the population based on their probabilities.
+        Args:
+            population - population to select from
+            probabilities - probabilities of selection for each individual
+        Returns:
+            Selected individuals
+        """
+        return population[
+            np.random.choice(population.shape[0], replace=False, p=probabilities,
+                             size=(floor(len(population) / 2), 2))]
 
-    def selection(self, probabilities):
-        self._paired_population = self._population[
-            np.random.choice(self._population.shape[0], replace=False, p=probabilities,
-                             size=(floor(len(self._population) / 2), 2))]
+    def mutate(self, generation):
+        """
+        Applies mutation to a generation of individuals.
+        Args:
+            generation - population to mutate
+        Returns:
+            Mutated population
+        """
+        mut_prob = 0.1
+        mutated_individuals = np.repeat(np.random.random(size=(generation.shape[0], 1)), 2, axis=1)
+        gaussian_noise = np.random.normal(0, 1, size=(floor(self._mi / 2) * self._lamb, 2))
+        return generation + (mutated_individuals < mut_prob) * gaussian_noise
 
+    def get_new_population(self, whole_population, whole_population_rate):
+        """
+        Selects the top individuals from a combined population.
+        Args:
+            whole_population - combined population
+            whole_population_rate - fitness values of the combined population
+        Returns:
+            New population and their normalized fitness values
+        """
+        sorted_indexes = np.argsort(whole_population_rate)[::True - (1-True)][:self._mi]
+        new_population = whole_population[sorted_indexes]
+        new_population_rate = whole_population_rate[sorted_indexes]
+        return new_population, new_population_rate * 1 / sum(new_population_rate)
 
-    def mutate(self):
-        mut_prob  = 0.1
-        mutated_individuals = np.repeat(np.random.random(size = (self._new_generation.shape[0],1)),2,axis=1)
-        gaussian_noise = np.random.normal(0, 1, size=(floor(self._mi/2) * self._lamb, 2))
-        self._mutated_generation = self._new_generation + (mutated_individuals < mut_prob) * gaussian_noise
-    def evolution(self):
+    def evolve(self):
+        """
+        Main evolution loop of the algorithm.
+        Returns:
+            The best individual found by the algorithm
+        """
         i = 0
-
+        population = self.set_starting_points(self._mi)
+        population_rate = self.fitness(population)
         while i < self._max_iter:
-            pass
+            paired_population = self.selection(population=population, probabilities=population_rate)
+            new_generation = self.crossover(parents=paired_population)
+            mutated_new_generation = self.mutate(new_generation)
+            whole_population = np.concatenate((population, mutated_new_generation), axis=0)
+            whole_population_rate = self.fitness(whole_population)
+            population, population_rate = self.get_new_population(whole_population, whole_population_rate)
+            i += 1
+        return population[1, :]
 
 
 def func(x: np.array, y: np.array) -> np.dtype:
@@ -131,20 +198,12 @@ def set_logging(folder_name: str) -> None:
 
 
 def main():
-    x = np.arange(-3, 3, 0.01)
-    y = np.arange(-3, 3, 0.01)
-    seed = 28
+    x = np.arange(-7, 7, 0.01)
+    y = np.arange(-7, 7, 0.01)
     set_logging("report")
-    evolution = EvolutionStrategy(mi=120, lamb=3, x=x, y=y, seed=28, func=func, max_iter=1000)
-    population = evolution.set_starting_points(120)
-    fit = evolution.fitness()
-    # evolution.mutate()
-    evolution.selection(fit)
-    evolution.crossover()
-    evolution.mutate()
-    # print(sel)
-    # print(fit)
-    # display_3d_function(x, y, func)
+    evolution = EvolutionStrategy(mi=3, lamb=120, x=x, y=y, seed=None, funct=func, max_iter=10)
+    result_point = evolution.evolve()
+    display_3d_function(x, y, func, result_point)
 
 
 if __name__ == "__main__":
